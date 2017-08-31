bd080901e0ee0b5768e2d712610073ad5c6a49ee
jsyoon <jsyoon@shinwooend.co.kr>
2017-08-31 14:09:35 +0900

    추가 Icon and Connect text

--- app/src/main/java/com/jsyoon/slepgrassdbg/MainActivity.java
+++ app/src/main/java/com/jsyoon/slepgrassdbg/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        getSupportActionBar().setSubtitle(R.string.msg_not_connected);
     }
 
     @Override
